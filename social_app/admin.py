709014b484a88478c71bb0e4062b9d67d7d5267d
Volodymyr Bohomol <8773927@gmail.com>
2023-09-11 15:07:08 +0300

    - registered Profile, Post, Comment, Like models on admin page

--- social_app/admin.py
+++ social_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from social_app.models import Post, Comment, Profile, Like
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+
+admin.site.unregister(Group)
