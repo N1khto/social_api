36634e08ef4b21330354787c21c3d1abbd73bd49
Volodymyr Bohomol <8773927@gmail.com>
2023-09-25 15:26:54 +0300

    - added .env file with DJANGO_SECRET_KEY - edit settings to load SECRET_KEY from environmental variables - added .env.sample file

--- social_api/settings.py
+++ social_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,9 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-eb_+@%8p^#guw&0+&cyq0x84bp8g$5j5m7b&t$17pn&5%+_*&4"
-)
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
